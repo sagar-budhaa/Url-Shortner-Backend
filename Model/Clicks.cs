77e70c4449ecb3547976ec5694dd524e23b84929
Sagar Budha <contact@sagarbudha.site>
2026-06-06 22:23:59 +0545

    created Click Model

---
+++ Model/Clicks.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Url_Shortner_Backend.Model;
+
+public class Clicks
+{
+    [Key] public Guid ClickId { get; set; } = Guid.NewGuid();
+    
+    [Required]
+    public string IpAddress { get; set; }
+    [Required]
+    public string Country { get; set; }
+    [Required]
+    public string UserAgent { get; set; }
+    [Required]
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    [Required]
+    public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    
+    [Required]
+    public Guid UrlId { get; set; }
+}
