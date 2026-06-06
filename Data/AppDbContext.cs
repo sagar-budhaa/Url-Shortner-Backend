104a171f60e5cc4f56e6bff5f08bff012f4854b1
Sagar Budha <contact@sagarbudha.site>
2026-06-06 22:19:53 +0545

    add appdbcontext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Url_Shortner_Backend.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options ) : DbContext(options)
+{
+    
+    
+}
