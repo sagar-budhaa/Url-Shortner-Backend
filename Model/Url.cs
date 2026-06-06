6ceb1563c2dfe06d3d3a190f953ddf7535595f17
Sagar Budha <contact@sagarbudha.site>
2026-06-06 22:20:04 +0545

    made url model

---
+++ Model/Url.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Url_Shortner_Backend.Model;
+
+[Index(nameof(ShortUrl), IsUnique = true)]
+public class Url
+{
+    [Key] public Guid Id { get; set; } = Guid.NewGuid();
+    [Required]
+    public string OriginalUrl { get; set; }
+    [Required]
+    public string ShortUrl { get; set; }
+    [Required]
+    public DateTime CreatedAt { get; set; }
+    [Required]
+    public DateTime UpdatedAt { get; set; }
+    
+    public bool IsActive { get; set; }  = true;
+}
