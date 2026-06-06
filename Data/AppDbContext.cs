3f7b6a2e6344a1f484a879d678a6d5dcbe43af12
Sagar Budha <contact@sagarbudha.site>
2026-06-06 22:27:14 +0545

    updated appdbcontext with dbset of url and click

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Url_Shortner_Backend.Model;
 
 namespace Url_Shortner_Backend.Data;
 
 public class AppDbContext(DbContextOptions<AppDbContext> options ) : DbContext(options)
 {
-    
+    public DbSet<Url> Urls { get; set; }
+    public DbSet<Click> Clicks { get; set; }
     
 }
