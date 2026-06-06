6e61e6414092fbea368e45c8c0da9030433c0672
Sagar Budha <contact@sagarbudha.site>
2026-06-06 23:15:00 +0545

    added GlobalExceptionHandler.cs

---
+++ Middleware/GlobalExceptionHandler.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Url_Shortner_Backend.DTOs.GlobalErrorHandler;
+
+namespace Url_Shortner_Backend.Middleware;
+
+public class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger) : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        logger.LogError(exception, "Unhandled exception: {Message}", exception.Message);
+
+        var statusCode = exception switch
+        {
+            ArgumentNullException => StatusCodes.Status400BadRequest,
+            UnauthorizedAccessException => StatusCodes.Status401Unauthorized,
+            KeyNotFoundException => StatusCodes.Status404NotFound,
+            _ => StatusCodes.Status500InternalServerError
+        };
+
+        var title = statusCode switch
+        {
+            StatusCodes.Status400BadRequest => "Bad Request",
+            StatusCodes.Status401Unauthorized => "Unauthorized",
+            StatusCodes.Status404NotFound => "Not Found",
+            _ => "Internal Server Error"
+        };
+        
+        var exceptionResponse = new ExceptionResponseHandlerDto
+        {
+            Status = statusCode,
+            Title = title,
+            Detail = exception.Message
+        };
+
+        httpContext.Response.StatusCode = (int)statusCode;
+
+        await httpContext.Response.WriteAsJsonAsync(exceptionResponse, cancellationToken);
+
+        return true;
+    }
+}
