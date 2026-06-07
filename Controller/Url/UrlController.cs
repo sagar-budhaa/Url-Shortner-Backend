a6191a7d6cb0c862971ae33fe7b18f02679028d3
Sagar Budha <contact@sagarbudha.site>
2026-06-07 12:50:48 +0545

    added Get Method

---
+++ Controller/Url/UrlController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Url_Shortner_Backend.DTOs.Url;
+using Url_Shortner_Backend.Service.Url;
+
+namespace Url_Shortner_Backend.Controller.Url;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UrlController(IUrlService urlService) : ControllerBase
+{
+
+    [HttpGet]
+    public ActionResult<List<UrlResponseDto>> Get()
+    {
+        var urls = urlService.GetAllUrls().Result;
+        return Ok(urls);
+    }
+    
+}
