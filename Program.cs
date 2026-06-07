b3956505113d799c180d746a09b3cc020f57638a
Sagar Budha <contact@sagarbudha.site>
2026-06-07 09:55:56 +0545

    inject scalar

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using System.Text;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +16,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
