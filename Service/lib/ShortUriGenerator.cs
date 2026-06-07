e3b18ae541aa2cd4aa99346b8a3c4c8304b05326
Sagar Budha <contact@sagarbudha.site>
2026-06-07 12:53:08 +0545

    created ShortUriGenerator

---
+++ Service/lib/ShortUriGenerator.cs
@@ -0,0 +1,17 @@
+﻿namespace Url_Shortner_Backend.Service.lib;
+
+public class ShortUriGenerator
+{
+    private const string AllowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_";
+    private static readonly Random Random = new();
+
+    public static string GenerateShortUri(int length = 6)
+    {
+        var shortUri = new char[length];
+        for (var i = 0; i < length; i++)
+        {
+            shortUri[i] = AllowedChars[Random.Next(AllowedChars.Length)];
+        }
+        return new string(shortUri);
+    }
+}
