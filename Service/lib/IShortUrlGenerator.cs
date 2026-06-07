5dd4391237d65db867c5d2b17cf777b7b195aecb
Sagar Budha <contact@sagarbudha.site>
2026-06-07 12:56:33 +0545

    updated shorturlgenerator

---
+++ Service/lib/IShortUrlGenerator.cs
@@ -0,0 +1,7 @@
+﻿namespace Url_Shortner_Backend.Service.lib;
+
+public interface IShortUrlGenerator
+{
+    public string GenerateShortUri(int length = 6);
+
+}
--- Service/lib/ShortUriGenerator.cs
+++ Service/lib/ShortUriGenerator.cs
@@ -1,11 +1,11 @@
 ﻿namespace Url_Shortner_Backend.Service.lib;
 
-public class ShortUriGenerator
+public class ShortUriGenerator : IShortUrlGenerator
 {
     private const string AllowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_";
     private static readonly Random Random = new();
 
-    public static string GenerateShortUri(int length = 6)
+    public string GenerateShortUri(int length = 6)
     {
         var shortUri = new char[length];
         for (var i = 0; i < length; i++)
