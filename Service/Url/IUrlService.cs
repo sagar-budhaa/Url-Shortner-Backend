a034fad7321dc46ca6164b108c3a8a16189a0764
Sagar Budha <contact@sagarbudha.site>
2026-06-07 09:51:15 +0545

    add Methods in IUrlService.cs

--- Service/Url/IUrlService.cs
+++ Service/Url/IUrlService.cs
@@ -1,6 +1,13 @@
-﻿namespace Url_Shortner_Backend.Service.Url;
+﻿using Url_Shortner_Backend.DTOs.Url;
+
+namespace Url_Shortner_Backend.Service.Url;
 
 public interface IUrlService
 {
+    public Task<List<UrlResponseDto>> GetAllUrls();
+    public Task<UrlResponseDto> GetOriginalUrl(string shortUrl);
+    public Task<UrlResponseDto> CreateShortUrl(UrlRequestDto urlRequestDto);
+    public Task<UrlResponseDto> UpdateShortUrl(UrlRequestDto urlRequestDto);
+    public Task<UrlResponseDto> DeleteShortUrl(string shortUrl);
     
 }
