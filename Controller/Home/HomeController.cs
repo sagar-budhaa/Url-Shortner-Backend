dfe514f16b8834dd4b3739d4935faebdeb682158
Sagar Budha <contact@sagarbudha.site>
2026-06-07 12:42:14 +0545

    added HomeController.cs

---
+++ Controller/Home/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Url_Shortner_Backend.Controller.Home;
+
+[ApiController]
+[Route("/")]
+public class HomeController : ControllerBase
+{
+    [HttpGet]
+    public RedirectResult Get()
+    {
+        return Redirect("scalar/v1");
+    }
+}
