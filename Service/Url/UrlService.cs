7055257a0efa8cc3371d9cc323516dd3a8cb81f1
Sagar Budha <contact@sagarbudha.site>
2026-06-07 09:52:39 +0545

    Implement IUrlService.cs in UrlService.cs

--- Service/Url/UrlService.cs
+++ Service/Url/UrlService.cs
@@ -1,6 +1,31 @@
-﻿namespace Url_Shortner_Backend.Service.Url;
+﻿using Url_Shortner_Backend.DTOs.Url;
 
-public class UrlService
+namespace Url_Shortner_Backend.Service.Url;
+
+public class UrlService : IUrlService
 {
-    
+    public Task<List<UrlResponseDto>> GetAllUrls()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UrlResponseDto> GetOriginalUrl(string shortUrl)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UrlResponseDto> CreateShortUrl(UrlRequestDto urlRequestDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UrlResponseDto> UpdateShortUrl(UrlRequestDto urlRequestDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UrlResponseDto> DeleteShortUrl(string shortUrl)
+    {
+        throw new NotImplementedException();
+    }
 }
